a0295b23818720d4cd5ec0e9534d6b7ef7844c28
Marco Antonio <marco@ASUS>
2017-01-09 19:42:01 -0200

    Modelagem e mapeamento de entidades com hibernate/jpa

--- src/main/java/com/cervejaria/brewer/model/Cerveja.java
+++ src/main/java/com/cervejaria/brewer/model/Cerveja.java
@@ -1,10 +1,31 @@
 package com.cervejaria.brewer.model;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.NotBlank;
 
-public class Cerveja {
+@Entity
+@Table(name="cerveja")
+public class Cerveja implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
 	
 	@NotBlank(message = "O Sku é obrigatório!")
 	private String sku;
@@ -14,6 +35,26 @@ public class Cerveja {
 	
 	@Size(max=50, min=1)
 	private String descricao;
+	
+	private BigDecimal valor;
+	
+	@Column(name="teor_alcoolico")
+	private BigDecimal teorAlcoolico;
+	
+	private BigDecimal comissao;
+	
+	@Column(name="quantidade_estoque")
+	private Integer quantidadeEstoque;
+	
+	@Enumerated(EnumType.STRING)
+	private Origem origem;
+	
+	@Enumerated(EnumType.STRING)
+	private Sabor sabor;
+	
+	@ManyToOne
+	@JoinColumn(name="codigo_estilo")
+	private Estilo estilo;
 	
 	public String getSku() {
 		return sku;
@@ -33,6 +74,79 @@ public class Cerveja {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public BigDecimal getTeorAlcoolico() {
+		return teorAlcoolico;
+	}
+	public void setTeorAlcoolico(BigDecimal teorAlcoolico) {
+		this.teorAlcoolico = teorAlcoolico;
+	}
+	public BigDecimal getComissao() {
+		return comissao;
+	}
+	public void setComissao(BigDecimal comissao) {
+		this.comissao = comissao;
+	}
+	public BigDecimal getValor() {
+		return valor;
+	}
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	public Integer getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+	public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+	public Origem getOrigem() {
+		return origem;
+	}
+	public void setOrigem(Origem origem) {
+		this.origem = origem;
+	}
+	public Sabor getSabor() {
+		return sabor;
+	}
+	public void setSabor(Sabor sabor) {
+		this.sabor = sabor;
+	}
+	public Estilo getEstilo() {
+		return estilo;
+	}
+	public void setEstilo(Estilo estilo) {
+		this.estilo = estilo;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cerveja other = (Cerveja) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+	
+	
 	
 	
 }
